e59931623ba10dc118ae7044b5c7aa96ef30c8ae
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-12 19:40:36 -0500

    feat: :sparkles: implementación del endpoint AlquilerCliente
    El endpoint AlquilerCliente se implementa para filtrar la solicitud con requerimientos especificos

--- routers/Alquiler.js
+++ routers/Alquiler.js
@@ -8,14 +8,68 @@ const appAlquiler = Router();
 let db = await con();
 let Alquiler = db.collection("Alquiler");
 
-appAlquiler.get("/", limitGrt(), appMiddlewareCampusVerify, async(req, res) => {
-    if(!req.rateLimit) return; 
+ appAlquiler.get("/todos", limitGrt(), appMiddlewareCampusVerify, async(req, res) => { //http://127.10.10.10:5011/Alquiler/todos
+     if(!req.rateLimit) return; 
+     console.log(req.rateLimit);
+     let db = await con();
+     let Alquiler = db.collection("Alquiler");
+     let result = await Alquiler.find({}).toArray();
+     res.send(result); });
+
+
+appAlquiler.get("/AlquilerCliente", limitGrt(), appMiddlewareCampusVerify, async (req, res) => { //http://127.10.10.10:5011/Alquiler/AlquilerCliente
+    if (!req.rateLimit) return;
     console.log(req.rateLimit);
-    let db = await con();
-    let Alquiler = db.collection("Alquiler");
-    let result = await Alquiler.find({}).toArray();
-    res.send(result);
-});
+  
+    try {
+      let db = await con();
+      let Alquiler = db.collection("Alquiler");
+  
+      let result = await Alquiler.aggregate([
+        { $match: { Estado: "Activo" } },
+        {
+          // Realizar un $lookup para obtener los datos de cliente relacionados
+          $lookup: {
+            from: "Cliente",
+            localField: "ID_Cliente",
+            foreignField: "ID_Cliente",
+            as: "cliente_data",
+          },
+        },
+        { $unwind: "$cliente_data" }, // Desenrollar el resultado del $lookup, ya que puede haber múltiples clientes relacionados para un alquiler
+        
+        // Agrupar los datos para obtener el resultado final
+        {
+          $group: {
+            _id: "$ID_Alquiler",
+            ID_Alquiler: { $first: "$ID_Alquiler" },
+            ID_Cliente: { $first: "$ID_Cliente" },
+            ID_Automovil: { $first: "$ID_Automovil" },
+            Fecha_Inicio: { $first: "$Fecha_Inicio" },
+            Fecha_Fin: { $first: "$Fecha_Fin" },
+            Costo_Total: { $first: "$Costo_Total" },
+            Estado: { $first: "$Estado" },
+            Cliente: {
+              $mergeObjects: {
+                Nombre: "$cliente_data.Nombre",
+                Apellido: "$cliente_data.Apellido",
+                DNI: "$cliente_data.DNI",
+                Direccion: "$cliente_data.Direccion",
+                Telefono: "$cliente_data.Telefono",
+                Email: "$cliente_data.Email",
+              },
+            },
+          },
+        },
+      ]).toArray();
+  
+      res.send(result);
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+      res.status(500).send("Error al obtener los datos");
+    }
+  });
+    
 appAlquiler.post("/", limitGrt(), appMiddlewareCampusVerify, appDTOData, async(req, res) => {
     let resul;
     try {
